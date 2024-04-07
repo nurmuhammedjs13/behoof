d57c430b404985886a256321c51efce39e823443
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-04-07 16:45:18 +0600

    Nurmuhammet changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const App = () =>{
   return (
     <>
-      <p>Hello world</p>
-      <p>Nurmuhammet star</p>
-      <p>Kanykei</p>
-      <p>eldiyar</p>
+    <p>Hello world</p>
+    <p>Nurmuhammet smooth</p>
+    <p>Nurmuhammet star</p>
+    <p>Kanykei</p>
     </>
   )
 }
