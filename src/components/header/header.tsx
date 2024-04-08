0228053427d15da8b6e8cbd674df3d8eb94f5b20
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-04-08 16:48:04 +0600

    masdter

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import headerLogo from '../../assets/header-logo.png'
+import headerLogo from "../../assets/header-logo.png";
+import "./header.css";
+import HeaderButtonHeart from "../../assets/heart.png";
+import HeaderButtonChart from "../../assets/chart.png";
+import HeaderButtonFrame from "../../assets/frame.png";
 
 function Header() {
     return (
@@ -10,12 +14,39 @@ function Header() {
                         <div className="header_content_info">
                             <div className="header_logo_content">
                                 <div className="header-logo">
-        <img src={headerLogo} alt="" className="header-logo-img"/>
+                                    <img
+                                        src={headerLogo}
+                                        alt=""
+                                        className="header-logo-img"
+                                    />
+                                    <h1 className="header_logo-text">Behoof</h1>
                                 </div>
+                                <h1 className="header-logo-info">
+                                    Лучшие цены в интернет-магазинах
+                                </h1>
                             </div>
-                        
+                            <div className="header_center_info_search_content">
+                                <div className="header_center_info_search_catalog">
+                                    Каталог товаров ↓
+                                </div>
+                                <input
+                                    placeholder="Поиск товаров"
+                                    type="text"
+                                    className="header_center_info_search_content-input"
+                                />
+                            </div>
+                            <div className="header_nav">
+                                <button className="header_nav-button">
+                                    <img src={HeaderButtonHeart} alt="" />
+                                </button>
+                                <button className="header_nav-button">
+                                    <img src={HeaderButtonChart} alt="" />
+                                </button>
+                                <button className="header_nav-button">
+                                    <img src={HeaderButtonFrame} alt="" />
+                                </button>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </section>
