5b727906ed60bd5d5d1bc83dbaa39084a3fe04bc
Tahmina <tahminakurmanalieva76@gmail.com>
2024-04-09 14:25:40 +0600

    Tahmina

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/header";
+import Section from "../../pages/home/section";
 
 function Home() {
-    return (
-        <>
-            <Header />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Section />
+    </>
+  );
 }
 
 export default Home;
