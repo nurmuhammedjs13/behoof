541b1681a3c2aa1a8dff4c98c14cf54a95e415d1
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-04-14 16:31:06 +0600

    finished work

--- src/components/newProduct/newProduct.tsx
+++ src/components/newProduct/newProduct.tsx
@@ -5,134 +5,145 @@ import HeaderButtonChart from "../../assets/chart.png";
 import img1 from "../../assets/2272131 1.png";
 
 function NewProduct() {
-  return (
-    <>
-      <div className="newProduct">
-        <div className="container">
-          <div className="newProduct__content">
-            <h3 className="newProduct__content-log">Новинки</h3>
-            <Link to={"/"} className="newProduct__content-newProduct">
-              К новинкам ➜
-            </Link>
-          </div>
-          <div className="newProduct__cards">
-            <ul className="newProduct__cards__card">
-              <li className="newProduct__cards__card-img">
-                <img src={img1} alt="" />
-              </li>
-              <li className="newProduct__cards__card-text">
-                object(Умные часы)
-              </li>
-              <li className="newProduct__cards__card-product">
-                object(Apple iPhone 13 Pro Max 256 ГБ серый)
-              </li>
-              <div className="newProduct__cards__card__info">
-                <li className="newProduct__cards__card__info-price">1100$</li>
-                <div className="newProduct__cards__card__info__icons">
-                  <Link
-                    to={"/favorites"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonHeart} alt="" />
-                  </Link>
-                  <Link
-                    to={"/differences"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonChart} alt="" />
-                  </Link>
+    return (
+        <>
+            <div className="newProduct">
+                <div className="container">
+                    <div className="newProduct__content">
+                        <h3 className="newProduct__content-log">Новинки</h3>
+                        <Link
+                            to={"/"}
+                            className="newProduct__content-newProduct"
+                        >
+                            К новинкам ➜
+                        </Link>
+                    </div>
+                    <div className="newProduct__cards">
+                        <ul className="newProduct__cards__card">
+                            <li className="newProduct__cards__card-img">
+                                <img src={img1} alt="" />
+                            </li>
+                            <li className="newProduct__cards__card-text">
+                                Умные часы
+                            </li>
+                            <li className="newProduct__cards__card-product">
+                                Apple iPhone 13 Pro Max 256 ГБ серый
+                            </li>
+                            <div className="newProduct__cards__card__info">
+                                <li className="newProduct__cards__card__info-price">
+                                    1100$
+                                </li>
+                                <div className="newProduct__cards__card__info__icons">
+                                    <Link
+                                        to={"/favorites"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonHeart} alt="" />
+                                    </Link>
+                                    <Link
+                                        to={"/differences"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonChart} alt="" />
+                                    </Link>
+                                </div>
+                            </div>
+                        </ul>
+                        <ul className="newProduct__cards__card">
+                            <li className="newProduct__cards__card-img">
+                                <img src={img1} alt="" />
+                            </li>
+                            <li className="newProduct__cards__card-text">
+                                Умные часы
+                            </li>
+                            <li className="newProduct__cards__card-product">
+                                Apple iPhone 13 Pro Max 256 ГБ серый
+                            </li>
+                            <div className="newProduct__cards__card__info">
+                                <li className="newProduct__cards__card__info-price">
+                                    1100$
+                                </li>
+                                <div className="newProduct__cards__card__info__icons">
+                                    <Link
+                                        to={"/favorites"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonHeart} alt="" />
+                                    </Link>
+                                    <Link
+                                        to={"/differences"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonChart} alt="" />
+                                    </Link>
+                                </div>
+                            </div>
+                        </ul>
+                        <ul className="newProduct__cards__card">
+                            <li className="newProduct__cards__card-img">
+                                <img src={img1} alt="" />
+                            </li>
+                            <li className="newProduct__cards__card-text">
+                                Умные часы
+                            </li>
+                            <li className="newProduct__cards__card-product">
+                                Apple iPhone 13 Pro Max 256 ГБ серый
+                            </li>
+                            <div className="newProduct__cards__card__info">
+                                <li className="newProduct__cards__card__info-price">
+                                    1100$
+                                </li>
+                                <div className="newProduct__cards__card__info__icons">
+                                    <Link
+                                        to={"/favorites"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonHeart} alt="" />
+                                    </Link>
+                                    <Link
+                                        to={"/differences"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonChart} alt="" />
+                                    </Link>
+                                </div>
+                            </div>
+                        </ul>
+                        <ul className="newProduct__cards__card">
+                            <li className="newProduct__cards__card-img">
+                                <img src={img1} alt="" />
+                            </li>
+                            <li className="newProduct__cards__card-text">
+                                Умные часы
+                            </li>
+                            <li className="newProduct__cards__card-product">
+                                Apple iPhone 13 Pro Max 256 ГБ серый
+                            </li>
+                            <div className="newProduct__cards__card__info">
+                                <li className="newProduct__cards__card__info-price">
+                                    1100$
+                                </li>
+                                <div className="newProduct__cards__card__info__icons">
+                                    <Link
+                                        to={"/favorites"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonHeart} alt="" />
+                                    </Link>
+                                    <Link
+                                        to={"/differences"}
+                                        className="newProduct__cards__card__info__icons-img"
+                                    >
+                                        <img src={HeaderButtonChart} alt="" />
+                                    </Link>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
-              </div>
-            </ul>
-            <ul className="newProduct__cards__card">
-              <li className="newProduct__cards__card-img">
-                <img src={img1} alt="" />
-              </li>
-              <li className="newProduct__cards__card-text">
-                object(Умные часы)
-              </li>
-              <li className="newProduct__cards__card-product">
-                object(Apple iPhone 13 Pro Max 256 ГБ серый)
-              </li>
-              <div className="newProduct__cards__card__info">
-                <li className="newProduct__cards__card__info-price">1100$</li>
-                <div className="newProduct__cards__card__info__icons">
-                  <Link
-                    to={"/favorites"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonHeart} alt="" />
-                  </Link>
-                  <Link
-                    to={"/differences"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonChart} alt="" />
-                  </Link>
-                </div>
-              </div>
-            </ul>
-            <ul className="newProduct__cards__card">
-              <li className="newProduct__cards__card-img">
-                <img src={img1} alt="" />
-              </li>
-              <li className="newProduct__cards__card-text">
-                object(Умные часы)
-              </li>
-              <li className="newProduct__cards__card-product">
-                object(Apple iPhone 13 Pro Max 256 ГБ серый)
-              </li>
-              <div className="newProduct__cards__card__info">
-                <li className="newProduct__cards__card__info-price">1100$</li>
-                <div className="newProduct__cards__card__info__icons">
-                  <Link
-                    to={"/favorites"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonHeart} alt="" />
-                  </Link>
-                  <Link
-                    to={"/differences"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonChart} alt="" />
-                  </Link>
-                </div>
-              </div>
-            </ul>
-            <ul className="newProduct__cards__card">
-              <li className="newProduct__cards__card-img">
-                <img src={img1} alt="" />
-              </li>
-              <li className="newProduct__cards__card-text">
-                object(Умные часы)
-              </li>
-              <li className="newProduct__cards__card-product">
-                object(Apple iPhone 13 Pro Max 256 ГБ серый)
-              </li>
-              <div className="newProduct__cards__card__info">
-                <li className="newProduct__cards__card__info-price">1100$</li>
-                <div className="newProduct__cards__card__info__icons">
-                  <Link
-                    to={"/favorites"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonHeart} alt="" />
-                  </Link>
-                  <Link
-                    to={"/differences"}
-                    className="newProduct__cards__card__info__icons-img"
-                  >
-                    <img src={HeaderButtonChart} alt="" />
-                  </Link>
-                </div>
-              </div>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default NewProduct;
