6b26c39249235482d95b198fc1a18faa588d07a1
MacBook_Air <macbook_air@MacBook-Air-MacBook-Air.local>
2024-04-15 12:16:30 +0600

    review finished

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,14 +2,17 @@ import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 import Hero from "../../components/hero/hero";
 import Header_content from "../../components/header_content/header_content";
+import ReviewPage from "../../components/review";
+
 
 function Home() {
   return (
     <>
       <Header />
       <Header_content />
-      <Hero />
-      <Footer />
+      <Hero /> 
+      <ReviewPage/>
+     <Footer />
     </>
   );
 }
