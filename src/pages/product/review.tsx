523eeb8ba4cc8723cfefe03e18a6cc0a5dba3952
Eldiar <eldiarkasymaliev94@gmail.com>
2024-04-15 15:24:13 +0600

    Eldiyar heros

--- src/pages/product/review.tsx
+++ src/pages/product/review.tsx
@@ -1,19 +1,19 @@
 // import React from "react";
 
 function review() {
-    return (
-        <div className="section">
-            <div className="container">
-                <div className="section_content">
-                    <div className="section_content_info">
-                        <div className="section_logo_content">
-                            <div className="section_logo"></div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="section_content">
+          <div className="section_content_info">
+            <div className="section_logo_content">
+              <div className="section_logo"></div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default review;
