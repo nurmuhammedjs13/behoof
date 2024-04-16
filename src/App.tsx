a87ef2e257713ad4beff085cd1ce57520d5a349c
Eldiar <eldiarkasymaliev94@gmail.com>
2024-04-16 15:30:31 +0600

    develop  pageNot

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,8 @@ import Profile from "./pages/profile/profile";
 import Differences from "./pages/differences/differences";
 import Favorites from "./pages/favorites/favorites";
 import GroupOfProducts from "./pages/groupOfProducts/groupOfProducts";
-<<<<<<< HEAD
 import InfoOfProfuct from "./pages/infoProduct/infoProduct";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-    },
-    {
-        path: "/differences",
-        element: <Differences />,
-    },
-    {
-        path: "/favorites",
-        element: <Favorites />,
-    },
-    {
-        path: "/groupOfProducts",
-        element: <GroupOfProducts />,
-    },
-    {
-        path: "/infofProduct",
-        element: <InfoOfProfuct />,
-    },
-=======
 import PageNot from "./pages/pageNot/pageNot";
 
 const router = createBrowserRouter([
@@ -57,20 +30,23 @@ const router = createBrowserRouter([
     element: <GroupOfProducts />,
   },
   {
+    path: "/infofProduct",
+    element: <InfoOfProfuct />,
+  },
+  {
     path: "/pageNot",
     element: <PageNot />,
   },
->>>>>>> d097ec35ee4a301e2786d55d1308d3b071b1d529
 ]);
 
 // const [show, setShow] = useState(true);
 
 function App() {
-    return (
-        <>
-            <RouterProvider router={router} />
-        </>
-    );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
