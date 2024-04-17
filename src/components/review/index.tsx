0dded6a0fdf2c9afecd55ed3bab544860352573d
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-04-17 14:33:20 +0600

    finished

--- src/components/review/index.tsx
+++ src/components/review/index.tsx
@@ -4,31 +4,20 @@ import "./style.css";
 
 function ReviewPage() {
     return (
-        <div className="container">
-            <p className="review__text">Отзывы</p>
-            <div className="card">
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-                <Review />
-            </div>
-            <div className="btnToCenter">
-                <button className="seeMore">Показать еще</button>
+        <div className="reviewpage">
+            <div className="container">
+                <p className="review__text">Отзывы</p>
+                <div className="card">
+                    <Review />
+                    <Review />
+                    <Review />
+                    <Review />
+                    <Review />
+                    <Review />
+                </div>
+                <div className="btnToCenter">
+                    <button className="seeMore">Показать еще</button>
+                </div>
             </div>
         </div>
     );
